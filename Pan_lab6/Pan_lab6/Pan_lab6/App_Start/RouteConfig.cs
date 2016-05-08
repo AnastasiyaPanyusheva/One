20f4cc8d54feca54b1db5688f71a79f49d1325ba
AnastasiyaPanyusheva <anastasiya-p95@mail.ru>
2016-05-08 12:29:51 +0500

    All

---
+++ Pan_lab6/Pan_lab6/Pan_lab6/App_Start/RouteConfig.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Pan_lab6
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "CreatePage",
+                url: "Create",
+                defaults: new { controller = "Notebook", action = "CreatePage" }
+            );
+
+            routes.MapRoute(
+               name: "Default",
+               url: "",
+               defaults: new { controller = "Notebook", action = "Index" }
+           );
+
+            routes.MapRoute(
+                name: "Save",
+                url: "Save",
+                defaults: new { controller = "Notebook", action = "Save", id = UrlParameter.Optional}
+            );
+
+            routes.MapRoute(
+                name: "Image",
+                url: "Image",
+                defaults: new { controller = "Notebook", action = "Image" }
+            );
+
+            routes.MapRoute(
+                name: "Load",
+                url: "Load",
+                defaults: new { controller = "Notebook", action = "Load" }
+            );
+
+            routes.MapRoute(
+                name: "Select",
+                url: "{name}",
+                defaults: new { controller = "Notebook", action = "Select", name = "{name}" }
+            );
+        }
+    }
+}
---
+++ Pan_lab6/Pan_lab6/Pan_lab6/Models/Images.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Pan_lab6.Models
+{
+    public static class Images
+    {
+        public static Image ToImage(this string text)
+        {
+            Image bmp = new Bitmap(1, 1);
+
+            var drawing = Graphics.FromImage(bmp);
+            var font = new Font("Times New Roman", 14);
+            var textColor = Color.Black;
+            var backColor = Color.White;
+            var textSize = drawing.MeasureString(text, font);
+
+            bmp.Dispose();
+            drawing.Dispose();
+
+            bmp = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            Brush textBrush = new SolidBrush(textColor);
+
+            drawing = Graphics.FromImage(bmp);
+            drawing.Clear(backColor);
+            drawing.DrawString(text, font, textBrush, 0, 0);
+            drawing.Save();
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            return bmp;
+        }
+
+        public static Stream ToStream(this Image image)
+        {
+            var Stream = new MemoryStream();
+            image.Save(Stream, ImageFormat.Png);
+            Stream.Position = 0;
+
+            return Stream;
+        }
+    }
+}
